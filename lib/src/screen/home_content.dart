db0233b4d1ea89d255f7418de0eca24c6617e429
ratchakxrn <74697758+NNNXT@users.noreply.github.com>
2022-03-19 16:56:27 +0700

    last update

--- lib/src/screen/home_content.dart
+++ lib/src/screen/home_content.dart
@@ -1,11 +1,22 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:skeletons/skeletons.dart';
 
 class HomeContent extends StatelessWidget {
   const HomeContent({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final List<String> imgList = [
+      'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+      'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+      'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+      'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+      'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+    ];
     return SafeArea(
       child: SingleChildScrollView(
         child: Padding(
@@ -24,10 +35,27 @@ class HomeContent extends StatelessWidget {
                 color: Colors.grey,
               ),
               const SizedBox(height: 16),
-              Container(
-                height: 140,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.orange,
+              CarouselSlider(
+                options: CarouselOptions(
+                  disableCenter: true,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 5),
+                ),
+                items: imgList
+                    .map(
+                      (item) => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: CachedNetworkImage(
+                          imageUrl: item.toString(),
+                          placeholder: (_, __) => const SkeletonAvatar(),
+                          errorWidget: (_, __, ___) => const Icon(
+                            Icons.error,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
               ),
               const SizedBox(height: 16),
               StreamBuilder(
